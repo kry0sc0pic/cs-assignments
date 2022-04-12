828c055e30e840cb221f9015073095e5044ae8c6
BuildTools <unconfigured@null.spigotmc.org>
2022-04-12 22:05:23 +0530

    Fixed #1

--- question1.1.py
+++ question1.1.py
@@ -6,5 +6,5 @@ which is divisble by n including m and n. Take m and n as input.
 n = int(input('Enter End Number: '))
 m = int(input('Enter Divisor Number: '))
 
-for i in range(n+1):
+for i in range(2,n+1):
     print(i if i%m==0 else '')
