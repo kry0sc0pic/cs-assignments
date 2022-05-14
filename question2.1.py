68faa74db0ff9287d4528ac2f4d17e9e48a80db8
Krishaay Jois <krishaayjois@gmail.com>
2022-05-14 20:56:14 +0530

    Added bugfixes

--- question2.1.py
+++ question2.1.py
@@ -10,12 +10,14 @@ function should return a 3 digit number generated randomly.
 
 import random
 def generate(n: int = 3) -> int:
+    if n is None:
+        n = 3
     num = ""
     for _ in range(n):
         num+=str(random.randint(0, 9) if len(num) > 0 else random.randint(1, 9))
     return int(num)
 
 # Run with input
-print(generate(int(input("Enter Number: "))))
+print(generate(int(i) if  (i := input("Enter Number: ")) else None))
 # Run without input
 # print(generate())
